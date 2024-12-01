c88f99d35f67a85e686df931515ba047cf71fdd1
vardhan <harshwardhankalse03@gmail.com>
2024-12-01 23:35:01 +0530

    seprating profile apis

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import connectDB from "./db/connection";
 import groceryRoutes from "./routes/groceryRoutes/groceries.routes";
 import authRoutes from "./routes/authRoutes/auth.routes";
 import orderRoutes from "./routes/orderRoutes/orders.routes";
+import profileRoutes from "./routes/authRoutes/profile.routes";
+import { authenticateJWT } from "./utils/middlewares/Jwt.middleware";
 
 const PORT = 5555;
 const app = express();
@@ -17,9 +19,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/grocery", groceryRoutes);
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/orders", orderRoutes);
+app.use("/api/v1/grocery", authenticateJWT, groceryRoutes);
+app.use("/api/v1/orders", authenticateJWT, orderRoutes);
+app.use("/api/v1/profile", authenticateJWT, profileRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
